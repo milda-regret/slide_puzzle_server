1a543e016e2851665d9247c6f1831c7c61e0bff7
jiyu <wkdduq4840@gmail.com>
2023-06-05 15:18:53 +0900

    add GET /user and GET /clear APIs

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, json, jsonify, send_from_directory, request
 #from werkzeug.serving import WSGIRequestHandler
 #WSGIRequestHandler.protocol_version = "HTTP/1.1"
 
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, world!'
 
-puzzles = {
-    'puzzle1' : ['png', '에헬즈'],
-    'puzzle2' : ['png', '에헬즈'],
-    'puzzle3' : ['png', '에헬즈'],
-    'puzzle4' : ['png', '에헬즈'],
-    'puzzle5' : ['jpg', '에헬즈'],
-    'puzzle6' : ['jpg', '에헬즈'],
-    'puzzle7' : ['jpg', '에헬즈'],
-}
+puzzles = { 'size' : 7, 'puzzle' : [
+    { 'name' : 'puzzle1', 'type' : 'png', 'contributor': '에헬즈' },
+    { 'name' : 'puzzle2', 'type' : 'png', 'contributor': '에헬즈' },
+    { 'name' : 'puzzle3', 'type' : 'png', 'contributor': '에헬즈' },
+    { 'name' : 'puzzle4', 'type' : 'png', 'contributor': '에헬즈' },
+    { 'name' : 'puzzle5', 'type' : 'jpg', 'contributor': '에헬즈' },
+    { 'name' : 'puzzle6', 'type' : 'jpg', 'contributor': '에헬즈' },
+    { 'name' : 'puzzle7', 'type' : 'jpg', 'contributor': '에헬즈' },
+]}
 
 @app.route('/puzzles')
 def get_puzzle_list():
@@ -24,11 +24,57 @@ def get_puzzle_list():
 
 @app.route('/image/<image_name>')
 def get_image(image_name):
-    return send_from_directory('static', image_name, as_attachment=True)
+    return send_from_directory('static', image_name, as_attachment=False)
 
 @app.route('/user')
 def get_user_profile():
-    pass
+    id = request.args.get('id')
+    if (id is None):
+        return 'usage: /user?id=<user id>'
+    
+    user = None
+    user_list: list = list()
+    with open('./list.json', 'r') as fd:
+        user_list = json.load(fd)
+        for u in user_list:
+            if (u['id'] == id):
+                user = u
+                break
+
+        # 등록되지 않은 사용자
+    if (user is None):
+        user = {'id': id, 'name': 'User', 'solved puzzle': []}
+        user_list.append(user)
+        with open('./list.json', 'w') as fd:
+            json.dump(user_list, fd)
+    
+    return jsonify(user)
+
+@app.route('/clear')
+def clear_puzzle():
+    id = request.args.get('id')
+    puzzle = int(request.args.get('puzzle'))
+
+    user = None
+    user_list: list = list()
+    with open('./list.json', 'r') as fd:
+        user_list = json.load(fd)
+        for u in user_list:
+            if (u['id'] == id):
+                user = u
+                break
+
+    if (user is None):
+        return 'No user'
+    
+    for x in user['solved puzzle']:
+        if (x == puzzle):
+            return 'saved'
+    
+    user['solved puzzle'].append(puzzle)
+    with open('./list.json', 'w') as fd:
+        json.dump(user_list, fd)
+    return 'saved'
 
 if __name__ == '__main__':
     app.run(host='192.168.0.2', port=2456, debug=True)
